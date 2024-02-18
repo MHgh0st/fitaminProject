fd82a640fd84f06faa230d9dfe67e57d9e4c788c
MHgh0st <mhgh0st.982@gmail.com>
2024-02-18 20:01:10 +0330

    bugs of mobile-app section fixed

--- src/sections/mobile-app.vue
+++ src/sections/mobile-app.vue
@@ -3,13 +3,13 @@
 </script>
 
 <template>
-  <div class="container flex items-center md:px-0 px-4">
-    <div class="md:w-1/2 w-full flex flex-col text-neutral md:justify-start justify-center">
+  <div class="container flex items-center md:px-0 px-4 lg:mt-0 mt-10">
+    <div class="md:w-1/2 w-full flex flex-col text-neutral text-center md:text-start">
       <h3 class="font-bold lg:text-5xl text-4xl">اپلیکیشن فیتامین</h3>
       <p class="font-medium lg:text-lg text-sm pt-5">با نصب اپلیکیشن فیتامین روی موبایل اندرویدی یا اپل خود میتونید در هر زمان
         و مکان به برنامه ورزشی تون دسترسی داشته باشید ، به صورت مستقیم با
         مربی در تماس باشید و از بانک حرکات فیتامین استفاده کنید.</p>
-      <div class="grid xl:grid-cols-4 grid-cols-2 pt-8 gap-4 justify-items-center lg:w-80 xl:w-full">
+      <div class="grid xl:grid-cols-4 grid-cols-2 pt-8 gap-4 lg:w-96 xl:w-full">
         <button class="btn btn-secondary flex justify-around text-neutral text-lg  rounded-xl w-full"><span
             class=" font-medium ">گوگل پلی</span>
           <font-awesome-icon icon="fa-brands fa-google-play"/>
